89987cf66c91ca19fc4867ed429d5050c76c4ed6
Amosh Dirsipe <amoshdirsipe0209@gmail.com>
2025-07-17 20:04:02 +0530

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,17 @@
+//Dialpg boxes
+alert
+prompt
+confirm
+alert("Welcome!");
+
+  var female = confirm("Are you a female")
+
+  if(female){
+    let name = prompt("Enter your name:");
+    document.writeln("Hello madam "  + name+ "welcome to my page")
+  }
+  else{
+
+    let name = prompt("Enter your name:");
+    document.writeln("Hello Mr " + name+ "welcome to my page")
+  }
